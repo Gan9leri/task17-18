926f922b5f448b11a460b65b7585b80d9989a638
AOvsyannikov <aovsyannikov@it-one.ru>
2024-06-27 21:45:27 +0300

    updated build.gradle DemoQATests

--- src/test/java/tests/DemoQATests.java
+++ src/test/java/tests/DemoQATests.java
@@ -4,8 +4,9 @@ import helpers.WithLogin;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.Tag;
 @DisplayName("Тесты для demoqa")
+@Tag("form")
 public class DemoQATests extends TestBase{
 
     @DisplayName("Тест удаления книги из профиля")
